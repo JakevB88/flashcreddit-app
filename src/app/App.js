ba711201d899bd54eb0f476fb9387716df3ccb97
JakevB88! <jvbezooijen1@gmail.com>
2026-03-11 17:28:39 +0000

    HashRouter

--- src/app/App.js
+++ src/app/App.js
@@ -1,18 +1,18 @@
 import React from "react";
-import {Route, BrowserRouter, Routes} from "react-router-dom";
+import {Route, HashRouterRouter, Routes} from "react-router-dom";
 import Posts from "../Features/posts/Posts.js";
 import Post from "../Features/posts/Post.js";
 import Comments from "../Features/comments/Comments.js";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Posts />}/>
         <Route path="posts" element={<Posts/>}/>
         <Route path="/posts/:id" element={<Post/>} />
         <Route path="/comments/:name" element={<Comments/>}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
